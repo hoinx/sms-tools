bbc483387f9fba5311cfec15d5d35955656f3c02
hoinx <joerg@hoin.de>
2015-12-09 22:34:19 -0500

    Update compare clustering Opt0 vs. Opt1

--- workspace/A10c/joe_Opt1_Cluster.py
+++ workspace/A10c/joe_Opt1_Cluster.py
@@ -2,9 +2,12 @@ import soundAnalysis as sa
 import matplotlib.pyplot as plt
 
 
+plt.figure(1)
+sa.doDescriptorPairScatterPlot('joeDown_Opt1', (3,13))
+#sa.clusterSounds('joeDown_Opt1', 2, [3,13])
 
-#sa.descriptorPairScatterPlot('joeDown_Opt1', (3,13))
+plt.figure(2)
+sa.doDescriptorPairScatterPlot('joeDown_Opt0', (3,13))
+#sa.clusterSounds('joeDown_Opt2', 2, [3,13])
 
-
-sa.clusterSounds('joeDown_Opt1', 2, [3,13])
-
+plt.show()
